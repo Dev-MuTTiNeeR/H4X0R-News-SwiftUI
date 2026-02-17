1ba1f6e10b562d68d232e9b755d57d638a736642
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-17 11:16:28 -0300

    Fixed some bug.

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List {
+                Text("post.title")
             }
             .navigationTitle("H4X0R NEWS")
         }
@@ -22,13 +25,3 @@ struct ContentView: View {
     ContentView()
 }
 
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
-
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Bonjour"),
-    Post(id: "3", title: "Hola")
-]
--- H4X0R News/Models/NetworkManager.swift
+++ H4X0R News/Models/NetworkManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    func fetchData() {
+        if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let result = try decoder.decode(Results.self, from: safeData)
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
