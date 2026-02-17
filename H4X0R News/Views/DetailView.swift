54829897dac609fbffe999f1f04a1327d7f09a4a
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-17 11:51:37 -0300

    Created WebView struct and putted in new swift file and added func to when you click some title, it goes on new page and shot the site about it.

---
+++ H4X0R News/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Cem Akkaya on 17/02/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ H4X0R News/Views/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  H4X0R News
+//
+//  Created by Cem Akkaya on 17/02/26.
+//
+
+import Foundation
