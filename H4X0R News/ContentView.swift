bff3bb8d5bccb914d9e4e1d8686d1614c35d59a3
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-17 10:52:40 -0300

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Cem Akkaya on 17/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Cem Akkaya on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
